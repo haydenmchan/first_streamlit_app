51cff8a1299ea072a791fd67751475ce00d45d8c
haydenmchan <128524334+haydenmchan@users.noreply.github.com>
2023-03-22 12:11:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ add_my_fruit =streamlit.text_input('What fruit would you like to add?')
 streamlit.write('User entered ',add_my_fruit)
 
 #test
-my_cur.execute("insert into fruit_load_list values ('from streamlit')"
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
